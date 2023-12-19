660a05464cc957c5f4c7388f6c919f75164fffb0
Arya Parashar <66545234+aryaparasharmrt@users.noreply.github.com>
2023-12-19 15:54:57 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.io.*;
+class Main{
+  public static void main(String [] args){
+    String str = "Hello java";
+    for(int i=str.length() -1 ; i>0; i--){
+      System.out.print(str);
+    }
+  }
+}
